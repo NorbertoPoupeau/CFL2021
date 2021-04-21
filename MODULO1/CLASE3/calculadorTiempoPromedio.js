e2bc00337446fc0dca08abc60dff02ef3720b628
NorbertoPoupeau <norbertopoupeau@hotmail.com>
2021-04-21 17:54:42 -0300

    correccion de indentacion clase3

--- MODULO1/CLASE3/calculadorTiempoPromedio.js
+++ MODULO1/CLASE3/calculadorTiempoPromedio.js
@@ -1,7 +1,7 @@
-let readlineSync = require("readline-sync");
-let vuelta1 = readlineSync.questionInt("ingrese tiempo vuelta 1");
-let vuelta2 = readlineSync.questionInt("ingrese tiempo vuelta 2");
-let vuelta3 = readlineSync.questionInt("ingrese tiempo vuelta 3");
-let vuelta4 = readlineSync.questionInt("ingrese tiempo vuelta 4");
-console.log("Tiempo Total",(vuelta1+vuelta2+vuelta3+vuelta4));
-console.log("Tiempo Promedio",(vuelta1+vuelta2+vuelta3+vuelta4)/4);
+let readlineSync = require ("readline-sync");
+let vuelta1 = readlineSync.questionInt ("ingrese tiempo vuelta 1");
+let vuelta2 = readlineSync.questionInt ("ingrese tiempo vuelta 2");
+let vuelta3 = readlineSync.questionInt ("ingrese tiempo vuelta 3");
+let vuelta4 = readlineSync.questionInt ("ingrese tiempo vuelta 4");
+console.log ("Tiempo Total",(vuelta1+vuelta2+vuelta3+vuelta4));
+console.log ("Tiempo Promedio",(vuelta1+vuelta2+vuelta3+vuelta4)/4);
