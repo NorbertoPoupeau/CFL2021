56f8209be73522be3157fd6e23cc1ed401b594b5
NorbertoPoupeau <norbertopoupeau@hotmail.com>
2021-04-27 15:43:29 -0300

    ejercicios While y  For, revisados

---
+++ MODULO1/CLASE6/ejercicio1.js
@@ -0,0 +1,13 @@
+let readlineSync = require ("readline-sync");
+let clave = readlineSync.question("indique clave: ");
+let contador
+for (contador=1;contador<4;contador++) {
+    if ((clave!="eureka") && (contador<4)){
+        
+        console.log ("clave incorrecta,vuelva a ingresar al sistema")
+        clave = readlineSync.question("indique clave: ")
+    } else {
+        console.log ("clave correcta, accede al sitema")
+    }
+}
+    
