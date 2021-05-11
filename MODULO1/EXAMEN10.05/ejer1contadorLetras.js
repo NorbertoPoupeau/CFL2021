37314fe7427eae2c8f7056d9cd66a5fdab04d745
NorbertoPoupeau <norbertopoupeau@hotmail.com>
2021-05-10 23:53:00 -0300

    no anda... lo subo asi para que quede registro horario... 23.52 hs

--- MODULO1/EXAMEN10.05/ejer1contadorLetras.js
+++ MODULO1/EXAMEN10.05/ejer1contadorLetras.js
@@ -2,19 +2,20 @@ let readlineSync=require("readline-sync");
 let textoOrigen=readlineSync.question("ingrese texto a analizar: ");//texto ingresado por usuario
 let cantidadCadaLetra=new Array(26);//arreglo que contiene la cantidad de cada letra en el texto
 let porcentajeCadaLetra=new Array(26);//contiene el porcentaje de cada letra contra el total del texto
-function contarLetras(textoOrigen,cantidadCadaLetra){//recorro el texto y cargo el arreglo con cada letra
+let n=textoOrigen.length//defino a la variable "n" como el tamaño del string
+function contarLetras(n,textoOrigen,cantidadCadaLetra){//recorro el texto y cargo el arreglo con cada letra
     let i;
-    textoOrigen.toLowerCase();//paso el texto completo a un solo tipo de letra
-    for(i=0;i<textoOrigen.length;i++){
-            if(textoOrigen[i]==a){
-                cantidadCadaLetra[i]++;
-            }
+    let textoTodoMinuscula=textoOrigen.toLowerCase().trim();//paso el texto completo a un solo tipo de letra
+    for(i=0;i<n;i++){
+            if(textoTodoMinuscula[i]=="a"){
+                cantidadCadaLetra[i]++;//si el contenido de textoTodoMinuscula en "i"es igual a "a"
+            }                          //que sume 1 en la posicion "i" del arreglo cantidadCadaLetra
         }
 }
-function calcularPorcentaje(textoOrigen,porcentajeCadaLetra,cantidadCadaLetra){
+function calcularPorcentaje(n,porcentajeCadaLetra,cantidadCadaLetra){
     let i;
-    for(i=0;i<textoOrigen.length();i++){
-        porcentajeCadaLetra[i]=cantidadCadaLetra[i]/textoOrigen.length()*100
+    for(i=0;i<n;i++){
+        porcentajeCadaLetra[i]=cantidadCadaLetra[i]/n*100
     }
 }
 contarLetras(textoOrigen,cantidadCadaLetra);
