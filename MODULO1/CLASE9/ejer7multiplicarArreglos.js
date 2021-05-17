609ef515f682f32e6072f4265f4f66b88bf43089
NorbertoPoupeau <norbertopoupeau@hotmail.com>
2021-05-17 00:44:48 -0300

    cambien length numerico por variable n... sigue sin multiplicar

--- MODULO1/CLASE9/ejer7multiplicarArreglos.js
+++ MODULO1/CLASE9/ejer7multiplicarArreglos.js
@@ -1,30 +1,31 @@
 let readlineSync=require("readline-sync");
-let v1=new Array(3);
-let v2=new Array(3);
-let vProducto1=new Array(3);
-let v3=new Array(3);
-let v4=new Array(3);
-let vProducto2=new Array(3);
-let vProducto1PorvProducto2=new Array(3);
-function multiplicarArreglos(v1,v2,vProducto1){
+let n=3;
+let v1=new Array(n);
+let v2=new Array(n);
+let vProducto1=new Array(n);
+let v3=new Array(n);
+let v4=new Array(n);
+let vProducto2=new Array(n);
+let vProducto1PorvProducto2=new Array(n);
+function multiplicarArreglos(v1,v2,vProducto1,n){
     let i;
-    for (i=0;i<3;i++){
-        v1[i]=readlineSync.questionInt("indique un numero para la posicion "+i+" del arreglo "+v1[3]+" :");
+    for (i=0;i<n;i++){
+        v1[i]=readlineSync.questionInt("indique un valor para la posicion "+i+" del arreglo: ");
     }
-    for (i=0;i<3;i++){   
-        v2[i]=readlineSync.questionInt("indique un numero para la posicion "+i+" del arreglo "+v2[3]+" :");
+    for (i=0;i<n;i++){   
+        v2[i]=readlineSync.questionInt("indique un valor para la posicion "+i+" del arreglo: ");
     }
         vProducto1[i]=v1[i]*v2[i];
-    return vProducto1[3];
+    console.log(vProducto1)
     }
-function multiplicarArreglosProducto(vProducto1,vProducto2,vProducto1PorvProducto2) {
+function multiplicarArreglosProducto(vProducto1,vProducto2,vProducto1PorvProducto2,n) {
     let i;
-    for(i=0;i<3;i++){
+    for(i=0;i<n;i++){
         vProducto1PorvProducto2[i]=vProducto1[i]*vProducto2[i];
     }
-    return vProducto1PorvProducto2;
+    console.log(vProducto1PorvProducto2[n])
 }
-multiplicarArreglos(v1,v2,vProducto1,3);
-multiplicarArreglos(v3,v4,vProducto2,3);
+multiplicarArreglos(v1,v2,vProducto1,n);
+multiplicarArreglos(v3,v4,vProducto2,n);
 console.log("el arreglo resultante tiene los siguientes valores: ");
-multiplicarArreglosProducto(vProducto1,vProducto2,vProducto1PorvProducto2);
+multiplicarArreglosProducto(vProducto1,vProducto2,vProducto1PorvProducto2,n);
