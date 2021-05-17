aa0435f59bf589b633c443179a04232282136e83
NorbertoPoupeau <norbertopoupeau@hotmail.com>
2021-05-16 23:49:28 -0300

    todavia no funciona...ver con Francisco

--- MODULO1/CLASE9/ejer7multiplicarArreglos.js
+++ MODULO1/CLASE9/ejer7multiplicarArreglos.js
@@ -9,12 +9,22 @@ let vProducto1PorvProducto2=new Array(3);
 function multiplicarArreglos(v1,v2,vProducto1){
     let i;
     for (i=0;i<3;i++){
-        v1[i]=readlineSync.questionInt("indique un numero para la posicion "+i+ ":");
-        v2[i]=readlineSync.questionInt("indique un numero para la posicion "+i+ ":");
-        vProducto1[i]=v1[i]*v2[i]
+        v1[i]=readlineSync.questionInt("indique un numero para la posicion "+i+" del arreglo "+v1[3]+" :");
     }
+    for (i=0;i<3;i++){   
+        v2[i]=readlineSync.questionInt("indique un numero para la posicion "+i+" del arreglo "+v2[3]+" :");
+    }
+        vProducto1[i]=v1[i]*v2[i];
+    return vProducto1[3];
+    }
+function multiplicarArreglosProducto(vProducto1,vProducto2,vProducto1PorvProducto2) {
+    let i;
+    for(i=0;i<3;i++){
+        vProducto1PorvProducto2[i]=vProducto1[i]*vProducto2[i];
+    }
+    return vProducto1PorvProducto2;
 }
 multiplicarArreglos(v1,v2,vProducto1,3);
 multiplicarArreglos(v3,v4,vProducto2,3);
 console.log("el arreglo resultante tiene los siguientes valores: ");
-multiplicarArreglos(vProducto1,vProducto2,vProducto1PorvProducto2,3);
+multiplicarArreglosProducto(vProducto1,vProducto2,vProducto1PorvProducto2);
