27815bb3035e2be1d53c8f5e94f9f8dc3c30ad5e
NorbertoPoupeau <norbertopoupeau@hotmail.com>
2021-05-18 22:58:37 -0300

    terminado carga por consola de arreglos

--- MODULO1/CLASE9/ejer7multiplicarArreglos.js
+++ MODULO1/CLASE9/ejer7multiplicarArreglos.js
@@ -1,5 +1,5 @@
 let readlineSync=require("readline-sync");
-let n=3;
+let n=2;
 let v1=new Array(n);
 let v2=new Array(n);
 let vProducto1=new Array(n);
@@ -7,25 +7,22 @@ let v3=new Array(n);
 let v4=new Array(n);
 let vProducto2=new Array(n);
 let vProducto1PorvProducto2=new Array(n);
-function multiplicarArreglos(v1,v2,vProducto1,n){
+function cargarArreglos(v1,n){
     let i;
-    for (i=0;i<n;i++){
+    for (i=0;i<=n;i++){
         v1[i]=readlineSync.questionInt("indique un valor para la posicion "+i+" del arreglo: ");
     }
-    for (i=0;i<n;i++){   
-        v2[i]=readlineSync.questionInt("indique un valor para la posicion "+i+" del arreglo: ");
-    }
-        vProducto1[i]=v1[i]*v2[i];
-    console.log(vProducto1)
-    }
-function multiplicarArreglosProducto(vProducto1,vProducto2,vProducto1PorvProducto2,n) {
-    let i;
-    for(i=0;i<n;i++){
-        vProducto1PorvProducto2[i]=vProducto1[i]*vProducto2[i];
-    }
-    console.log(vProducto1PorvProducto2[n])
 }
+function multiplicarArreglos(v1,v2,vProducto1,n){
+    for(i=0;i<=n;i++)
+    vProducto1[i]=v1[i]*v2[i];
+    console.log(vProducto1);
+}    
+cargarArreglos(v1,n);
+cargarArreglos(v2,n);
+cargarArreglos(v3,n);
+cargarArreglos(v4,n);
 multiplicarArreglos(v1,v2,vProducto1,n);
 multiplicarArreglos(v3,v4,vProducto2,n);
 console.log("el arreglo resultante tiene los siguientes valores: ");
-multiplicarArreglosProducto(vProducto1,vProducto2,vProducto1PorvProducto2,n);
+multiplicarArreglos(vProducto1,vProducto2,vProducto1PorvProducto2,n);
